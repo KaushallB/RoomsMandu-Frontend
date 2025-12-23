7ddb63ff323c12abd55f313a48b04326e4f43f2b
Kaushal Bikram Gajmer <kaushalbikram44@gmail.com>
2025-12-23 18:58:20 +0545

    Full login done

--- django-frontend/app/components/modals/LoginModal.tsx
+++ django-frontend/app/components/modals/LoginModal.tsx
@@ -5,25 +5,79 @@ import Modal from './Modal';
 import { useState } from 'react';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import CustomButton from '../forms/CustomButton';
+import { useRouter } from 'next/navigation';
+import { handleLogin } from '@/app/lib/actions';
+import apiService from '@/app/services/apiService';
 
 const LoginModal = () => {
-    const loginModal= useLoginModal()
+    const router= useRouter();
+    const loginModal=useLoginModal();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState<string[]>([]);
+
+    const submitLogin = async () => {
+        const formData = {
+            email: email,
+            password: password
+        }
+
+        try {
+            const response = await apiService.post('/api/v1/auth/login/', formData);
+
+            if (response.access) {
+                handleLogin(response.user.pk, response.access, response.refresh);
+
+                loginModal.close();
+
+                router.push('/');
+            } else {
+                setErrors(response.non_field_errors || ['Login failed']);
+            }
+        } catch (error) {
+            console.error('Login error:', error);
+            setErrors(['Login failed. Please try again.']);
+        }
+    }
 
     const content = (
         <>
             <h2 className='mb-6 text-2xl'> Please Login</h2>
 
-            <form className='space-y-3'>
-                <input type="email" placeholder=" Enter your e-mail Address" className='w-full h-[54px] border border-gray-300 rounded-xl' />
-                <input type="password" placeholder="Enter your Password" className='w-full h-[54px] border border-gray-300 rounded-xl' />
+            <form 
+                className='space-y-3'
+                onSubmit={(e) => { e.preventDefault(); submitLogin(); }}
+            >
+                <input 
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} 
+                    placeholder="Enter your e-mail Address" 
+                    className='w-full h-[54px] px-4 border border-gray-300 rounded-xl' 
+                />
+
+                <input 
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                    placeholder="Enter your Password" 
+                    className='w-full h-[54px] px-4 border border-gray-300 rounded-xl' 
+                />
                 
-                <div className='p-5 bg-red-500 text-white rounded-xl opacity-80'>
-                    Error Message
-                </div>
+                {errors.map((error, index) => {
+                    return (
+                        <div 
+                            key={`error_${index}`}
+                            className="p-5 bg-red-500 text-white rounded-xl opacity-80"
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
                 
                 <CustomButton
-                    label="Submit"
-                    onClick={()=>console.log('Clicked submit button')}
+                    label="Login"
+                    onClick={(e) => { e.preventDefault(); submitLogin(); }}
                 />
             </form>
         </>
--- django-frontend/app/lib/actions.ts
+++ django-frontend/app/lib/actions.ts
@@ -3,21 +3,23 @@
 import { cookies } from "next/headers";
 
 export async function handleLogin(userId: string, accessToken:string, refreshToken:string){
-    cookies().set('session_userid', userId, {
+    const cookieStore = await cookies();
+    
+    cookieStore.set('session_userid', userId, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24 *7,
         path: '/'
     });
 
-    cookies().set('session_refresh_token', refreshToken, {
+    cookieStore.set('session_refresh_token', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24 *7,
         path: '/'
     });
 
-    cookies().set('session_access_token', accessToken, {
+    cookieStore.set('session_access_token', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60,
